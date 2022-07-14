1322685a477a7f24ae02e75afb0b6c416e1be547
Niamh Gowran <91330763+ngowran@users.noreply.github.com>
2022-07-14 19:49:13 +0000

    Install MUI, create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import ResponsiveAppBar from './components/layout/navigation/navbar/navbar.jsx';
 
 function App() {
+  return <ResponsiveAppBar> </ResponsiveAppBar>;
   return (
     <main>
       Niamhs Dev Portfolio
