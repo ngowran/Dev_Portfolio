88ddda4fdfefadfdd60a1298591835731bb01d8c
Niamh Gowran <91330763+ngowran@users.noreply.github.com>
2022-07-19 16:17:03 +0000

    Style

--- src/components/layout/sections/about.jsx
+++ src/components/layout/sections/about.jsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
 
     return (
-      <H1>YAS</H1>
+       <motion.div
+   
+    whileHover={{ scale: 1.2, rotate: 90 }}
+    whileTap={{ scale: 0.8, rotate: -90, borderRadius: "100%" }}
+  />
     );
 };
 
